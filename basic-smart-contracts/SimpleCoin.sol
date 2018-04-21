4b3fe1fca07849427901d707cd3764fdc4ef8d95
Viet Le <vietlq85@gmail.com>
2018-04-21 15:41:06 +0100

    added notes for Ethereum contract logs

--- basic-smart-contracts/SimpleCoin.sol
+++ basic-smart-contracts/SimpleCoin.sol
@@ -55,6 +55,47 @@ function transfer(address _to, uint256 _value) public onlyOwner returns (bool)
 
 /*
 
+* https://codeburst.io/deep-dive-into-ethereum-logs-a8d2047c7371
+
+* https://ethereumbuilders.gitbooks.io/guide/content/en/ethereum_javascript_api.html#contract-events
+
+* https://blockgeeks.com/ethereum-smart-contract-clients/
+
+* https://coursetro.com/posts/code/100/Solidity-Events-Tutorial---Using-Web3.js-to-Listen-for-Smart-Contract-Events
+
+* https://blog.gridplus.io/scaling-blockchains-with-apache-kafka-814c85781c6
+
+* https://medium.com/etherereum-salon/eth-testing-472c2f73b4c3
+
+* https://programtheblockchain.com/dapps/counter-with-events/
+* https://programtheblockchain.com/dapps/multicounter/
+* https://programtheblockchain.com/posts/2018/01/24/logging-and-watching-solidity-events/
+
+* https://nethereum.readthedocs.io/en/latest/contracts/calling-transactions-events/
+
+*/
+
+/*
+
+Event topic ID is SHA3 of its signature!
+
+https://codeburst.io/deep-dive-into-ethereum-logs-a8d2047c7371
+
+> sha3('Pregnant(address,uint256,uint256,uint256)')
+241ea03ca20251805084d27d4440371c34a0b85ff108f6bb5611248f73818b80
+
+> sha3('Transfer(address,address,uint256)')
+ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef
+
+> sha3('Approval(address,address,uint256)')
+8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925
+
+> sha3('Birth(address,uint256,uint256,uint256,uint256)')
+0a5311bd2a6608f08a180df2ee7c5946819a649b204b554bb8e39825b2c50ad5
+
+> sha3('ContractUpgrade(address)')
+450db8da6efbe9c22f2347f7c2021231df1fc58d3ae9a2fa75d39fa446199305
+
 [
     {
         "topic": "ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
