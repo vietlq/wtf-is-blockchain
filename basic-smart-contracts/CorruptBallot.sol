175619bb958184e7e05301ae4b3ecea92ad2afbf
Viet Le <vietlq85@gmail.com>
2018-04-22 15:27:25 +0100

    payout to the voter once bribed vote is casted

--- basic-smart-contracts/CorruptBallot.sol
+++ basic-smart-contracts/CorruptBallot.sol
@@ -6,9 +6,11 @@ import "./ballot.sol";
 
 contract CorruptBallot is Ballot {
     // proposal => (voter => (briber => amount))
-    mapping (uint8 => mapping(address => mapping(address => uint256))) proposalVoterBribes;
+    mapping (uint8 => mapping (address => mapping (address => uint256))) proposalVoterBribes;
+    mapping (address => mapping (uint8 => uint256)) bribesOwed;
 
-    event BribeVoter(uint8 _proposal, address _from, address _to, uint256 _amount);
+    event VoterBribed(uint8 _proposal, address _from, address _to, uint256 _amount);
+    event VoterBribePaid(uint8 _proposal, address _to, uint256 _amount);
 
     modifier onlyValidVoterModifier(address _voter) {
         require(voters[_voter].voted == false && voters[_voter].weight > 0);
@@ -28,18 +30,34 @@ contract CorruptBallot is Ballot {
     // Access the amount via msg.value
     // To deposit, simply send a valid value of ETH to the contract when calling this function
     function bribe(address _to, uint8 _proposal) payable onlyValidProposalModifier(_proposal) onlyValidVoterModifier(_to) public {
-        if(proposalVoterBribes[_proposal][_to][msg.sender] > 0) {
-            proposalVoterBribes[_proposal][_to][msg.sender] += msg.value;
-        } else {
-            proposalVoterBribes[_proposal][_to][msg.sender] = msg.value;
-        }
+        proposalVoterBribes[_proposal][_to][msg.sender] += msg.value;
+        bribesOwed[_to][_proposal] += msg.value;
 
-        emit BribeVoter(_proposal, msg.sender, _to, msg.value);
+        emit VoterBribed(_proposal, msg.sender, _to, msg.value);
     }
 
     /// Give a single vote to proposal $(toProposal).
     // https://solidity.readthedocs.io/en/develop/common-patterns.html#withdrawal-from-contracts
-    function vote(uint8 _proposal) onlyValidProposalModifier(_proposal) public returns (bool _voteValid) {
-        super.vote(_proposal);
+    function vote(uint8 _proposal) onlyValidProposalModifier(_proposal) onlyValidVoterModifier(msg.sender) public returns (bool _voteValid) {
+        _voteValid = super.vote(_proposal);
+        assert(_voteValid == true);
+
+        // Pay the voter if she voted according to the bribe
+        address _voter = msg.sender;
+        uint256 _payout = bribesOwed[_voter][_proposal];
+        if (_payout > 0) {
+            // Transfer the payout to the voter
+            _voter.transfer(_payout);
+
+            // No more payout owed to the voter for this proposal
+            bribesOwed[_voter][_proposal] = 0;
+
+            // Record the payout
+            emit VoterBribePaid(_proposal, _voter, _payout);
+        }
+    }
+
+    function canVote(address _person) view public returns (bool) {
+        return (voters[_person].voted == false && voters[_person].weight > 0);
     }
 }
